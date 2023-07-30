370c4b54bc1e209e59030ee7dcf49f7c5d81b112
Shazzad Hossain Khan <sajib1223@gmail.com>
2023-07-31 02:38:43 +0600

    validated html field value

--- src/Field/Html.php
+++ src/Field/Html.php
@@ -2,14 +2,17 @@
 namespace Shazzad\WpFormUi\Field;
 
 class Html extends Field {
+
 	public function __construct( $data = [] ) {
 		$data['type'] = 'html';
 		parent::__construct( $data );
 	}
+
 	public function get_html( $form ) {
+		if ( ! isset( $this->html ) ) {
+			return '';
+		}
+
 		return $this->html;
 	}
 }
-
-?>
-
